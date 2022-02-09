190427608381944618fe0e0c7be1fca03af53b33
IrrealV <vmherasdurillo@hotmail.com>
2022-02-09 09:39:48 +0100

    correcion app

--- AppBasica/app/src/main/java/com/example/appbasica/MainActivity.kt
+++ AppBasica/app/src/main/java/com/example/appbasica/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.content.ContextCompat
 import java.awt.font.TextAttribute
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +19,24 @@ class MainActivity : AppCompatActivity() {
         var ocultar = findViewById<Switch>(R.id.ocultar)
         val cambiarTema = findViewById<ToggleButton>(R.id.cambiartema)
         var valorConver = findViewById<EditText>(R.id.valor_convertir)
+        var dinconv = findViewById<RadioGroup>(R.id.Conversor)
         var dolar = findViewById<RadioButton>(R.id.dolar)
         var euro = findViewById<RadioButton>(R.id.euros)
-        val conver = findViewById<Button>(R.id.conversor)
+        var conver = findViewById<Button>(R.id.conversor)
         val reset = findViewById<Button>(R.id.reinicio)
+        val fondres = findViewById<ImageView>(R.id.fondoresultado)
         var resconv = findViewById<TextView>(R.id.resultadoconver)
+
+        //
+        cambiarTema.setOnCheckedChangeListener { compoundButton, cheked ->
+            if(cheked){
+                val drawable = ContextCompat.getDrawable(this, R.drawable.fondoresultadonegro)
+                fondres.background = drawable
+            }else{
+                val drawable = ContextCompat.getDrawable(this, R.drawable.fondoresultado)
+                fondres.setBackgroundResource(R.drawable.fondoresultado)
+            }
+        }
 
         //Esto hace que se cree una ventana emergente diciendo la operacion que se hizo
         botonsum1.setOnClickListener{
@@ -42,10 +56,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //Esto hace que al introducir un numero los botenes sumen o resten uno a ese numero y lo pongan en el lugar del resultado
-        botonsum1.setOnClickListener {
-            chngnum
-        }
+        /*conver.setOnClickListener {
+            val valorString = valorConver.text.toString()
+
+            if (valorString.isEmpty()){
+                Toast.makeText(this, "El campo está vacio", Toast.LENGTH_SHORT).show()
+            }else{
+                val valor = valorString.toDouble()
+                if(dinconv.checkedRadioButtonId == dolar.isChecked){}
+            }
+
+
+
+        }*/
+
 
 
     }
