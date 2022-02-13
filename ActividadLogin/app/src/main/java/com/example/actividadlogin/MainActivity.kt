a419e940cf9e7af1e37dbe50813b31e09393e7c8
IrrealV <vmherasdurillo@hotmail.com>
2022-02-13 17:28:22 +0100

    Layout 2

--- ActividadLogin/app/src/main/java/com/example/actividadlogin/MainActivity.kt
+++ ActividadLogin/app/src/main/java/com/example/actividadlogin/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.actividadlogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "CICLO_DE_VIDA"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,11 +18,40 @@ class MainActivity : AppCompatActivity() {
         login.setOnClickListener {
             val cambio = Intent(this, SecondActivity::class.java)
 
-            cambio.putExtra("user", "victor.heras")
-            cambio.putExtra("pass", "qasdf")
+            cambio.putExtra("user","victor.heras" )
+            cambio.putExtra("pass", "qwerasdf")
 
             startActivity(cambio)
             finish()
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
     }
 }
--- ActividadLogin/app/src/main/java/com/example/actividadlogin/SecondActivity.kt
+++ ActividadLogin/app/src/main/java/com/example/actividadlogin/SecondActivity.kt
@@ -1,19 +1,29 @@
 package com.example.actividadlogin
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.enviar_info)
 
         val bundle = intent.extras
+
         if (bundle!!.containsKey("user") == true){
-            val usuario = bundle.getString("user")
+            val usuario = bundle.getString("user", "victor.heras")
             Toast.makeText(this, usuario, Toast.LENGTH_SHORT).show()
             }
+
+        val volver = findViewById<Button>(R.id.volver)
+        volver.setOnClickListener {
+            val cambio = Intent(this, MainActivity::class.java)
+
+        }
     }
 }
