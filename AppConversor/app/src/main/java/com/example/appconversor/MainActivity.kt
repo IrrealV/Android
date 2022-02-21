ee820d6d457d274f8046c17eb3c555a21eae5bbf
IrrealV <vmherasdurillo@hotmail.com>
2022-02-21 23:59:20 +0100

    Enfermo
    Probablemente mi salud mental está en juego por culpa de esta mierda

--- AppConversor/app/src/main/java/com/example/appconversor/MainActivity.kt
+++ AppConversor/app/src/main/java/com/example/appconversor/MainActivity.kt
@@ -2,44 +2,84 @@ package com.example.appconversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.example.appconversor.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
-
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity(), TextWatcher {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Esto hace que el EditTexView y el TextView cambien a la vez
+        binding.tiempo.addTextChangedListener(this)
+        //Esto hace que el EditTexView y el TextView cambien a la vez
 
 
-        val tempoCAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, )
-        binding.accion.adapter = tempoCAdapter
-        binding.accion.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                Toast.makeText(this@MainActivity, tiempocorto[p2], Toast.LENGTH_SHORT).show()
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
 
+
+
+
+
+        if(binding.tiempototal.text == null){
+            binding.tiempototal.text = "00:00"
         }
 
 
+    //Actividades que duran poco tiempo-------------------------------------------------------------
+        val tiempocorto = ArrayAdapter.createFromResource(
+            this,
+            R.array.tiempocorto,
+            R.layout.spinner_color
+        )
+        binding.accion.adapter = tiempocorto
+        tiempocorto.setDropDownViewResource(android.R.layout.select_dialog_singlechoice)
+    //Actividades que duran poco tiempo-------------------------------------------------------------
+
+    //Actividades que duran tiempo medio------------------------------------------------------------
+        val tiempomedio = ArrayAdapter.createFromResource(
+            this,
+            R.array.tiempomedio,
+            R.layout.spinner_color
+        )
+        binding.accion.adapter = tiempomedio
+        tiempomedio.setDropDownViewResource(android.R.layout.select_dialog_singlechoice)
+    //Actividades que duran tiempo medio------------------------------------------------------------
+
+    //Actividades que duran mucho tiempo------------------------------------------------------------
+        val tiempolargo = ArrayAdapter.createFromResource(
+            this,
+            R.array.tiempolargo,
+            R.layout.spinner_color
+        )
+        binding.accion.adapter = tiempolargo
+        tiempolargo.setDropDownViewResource(android.R.layout.select_dialog_singlechoice)
+    //Actividades que duran mucho tiempo------------------------------------------------------------
 
 
-        val tiempomedio = arrayOf("Partida", "Ducha","Tarea" )
+        }
 
-        val tiempolargo = arrayOf("Cocinar", "Programar","Siesta")
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+        }
 
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            binding.tiempototal.text = p0
+        }
 
+        override fun afterTextChanged(p0: Editable?) {
 
+        }
     }
-}
+
