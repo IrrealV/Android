3a95843550041d962e728a7c2cd474e4aea64a0f
IrrealV <vmherasdurillo@hotmail.com>
2022-04-28 10:21:45 +0200

    Ejercicios Gato

---
+++ AppGatos/app/src/main/java/com/example/appgatos/retrofit/Repositorio.kt
@@ -0,0 +1,10 @@
+package com.example.appgatos.retrofit
+
+import com.example.appgatos.dataclass.EnvioVoto
+
+class Repositorio {
+    private val retrofitService = RetrofitHelper.getRetrofit()
+
+    suspend fun todosLosGatos() = retrofitService.getCatsList()
+    suspend fun todosLosVotos() = retrofitService.getListaVotos(String())
+}
