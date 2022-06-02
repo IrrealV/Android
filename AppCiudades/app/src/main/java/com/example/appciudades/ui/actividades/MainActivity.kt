8f6cc37093a1757a041b7c08b1d8e39592f7086a
IrrealV <vmherasdurillo@hotmail.com>
2022-06-02 21:55:05 +0200

    Me voy a cortar la pija

--- AppCiudades/app/src/main/java/com/example/appciudades/ui/actividades/MainActivity.kt
+++ AppCiudades/app/src/main/java/com/example/appciudades/ui/actividades/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appciudades.ui.actividades
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -12,6 +13,7 @@ import com.example.appciudades.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +21,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val navController = findNavController(R.id.fragmentContainerView)
+        navController = findNavController(R.id.map)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragmentContainerView)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
